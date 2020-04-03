d850a98b1e9761a04215adff8aa68e25c76b41e9
Miguel Angel Garcia Ruiz <miagarciaru@unal.edu.co>
2020-04-02 22:48:03 -0700

    Funciones con numeros usando print

---
+++ print2.py
@@ -0,0 +1,25 @@
+from math import*
+
+print (3) #imprime 3
+print (3*(4+5))
+
+print (10 % 3) #regresa el módulo de 10%3 osea 1
+
+my_num = 5
+print (my_num)
+print (str(my_num) + " my favorite number")
+
+my_num=-5
+print (abs(my_num)) #valor absoluto
+
+print (pow(3,2)) #da 3 elevado a la dos
+
+print (max(4, 6)) #da el numero más grande
+print (min(4,6)) #el minimo de ambos
+print (round(3.7)) #redondea
+
+print (floor(3.7)) #funcion piso
+print (ceil(3.7))
+
+print (sqrt(36)) #raiz cuadrada
+
