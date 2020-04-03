4ffc4d7552bd96c6d3157114856ef06b15630130
Slackware Live User <live@darkstar.example.net>
2020-04-02 22:02:04 -0700

    Algunos comandos básicos de print

---
+++ print1.py
@@ -0,0 +1,27 @@
+print ("Hello Python") #Poner para escribir lo que se quiere decir
+
+print ("He was a man called John")
+print ("He had 70 years old until he died")
+
+character_name = "John"
+character_age = 50
+
+print ("He was a man called " + character_name)
+#print ("He had " + character_age +" years old until he died") had an error
+
+print ("Giraffe Academic")
+phrase = "Giraffe Academic"
+
+print (phrase)
+print (phrase.lower()) #imprime en minusculas
+print (phrase.upper()) #imprime en mayúsculas
+
+print (len(phrase)) #numero de caracteres de la variable frase
+print (phrase[0]) #letra correspondiente al espacio cero, osea G
+
+print (phrase.index("G")) #Retorna la posición de G
+print (phrase.index("Academic")) #Retorna donde se enuentra la primera letra
+
+print (phrase.replace("Giraffe", "Elephant")) #reemplaza la palabra Giraffe por Elephant
+
+
