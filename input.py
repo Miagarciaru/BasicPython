091d174d99407de12d51240b130924e269e0a06a
Miguel Angel Garcia Ruiz <miagarciaru@unal.edu.co>
2020-04-02 22:54:53 -0700

    Input estructura básica

---
+++ input.py
@@ -0,0 +1,5 @@
+
+name = input ("Enter your name: ") #pide una salida al usuario y lo graba en la variable name
+age = input ("Enter your name: ") #pide la edad del usuario y la guarda en age
+print ("Hello"   + name + "| You are " + age)
+
